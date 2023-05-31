898835b903f1da5d922b039e1a96d9cfeaa3ebf6
heayem <heayem7@gmail.com>
2023-06-01 00:59:05 +0700

    order update  product

--- Backend/src/Rout_Controller/OrderProduct.Controller.js
+++ Backend/src/Rout_Controller/OrderProduct.Controller.js
@@ -1,5 +1,4 @@
-// const { request } = require('express')
-const { json } = require('express')
+
 const db = require('../db.configer/db.configer')
 
 
@@ -7,65 +6,74 @@ const CreateOrderProduct = (req, res) => {
 
     let body = req.body
     let message = {}
-
-    if (body.User_Id == null || body.User_Id == "") {
-        message.User_Id = "Plz set User_Id"
+    if (body.cart_id == "" || body.cart_id == null) {
+        message.cart_id = "plz input cart_id"
     }
-    if (body.P_Id == null || body.P_Id == "") {
-        message.P_Id = "Plz input your product Id"
+    if (body.Discount == "" || body.Discount == null || body.Discount == undefined) {
+        message.Discount = "plz input Discount"
     }
-    if (body.Qty == null || body.Qty == "") {
-        message.Qty = "Plz set Qty "
+    if (body.order_num == "" || body.order_num == null || body.order_num == undefined) {
+        message.order_num = "plz input order_num"
     }
-
-    if (Object.keys(message).length > 0) {
+    if (body.order_status == "" || body.order_status == null || body.order_status == undefined) {
+        message.order_status = "plz input order_status"
+    }
+    if (Object.keys(message) > 0) {
         res.json({
             error: true,
             message: message
         })
         return false
     }
-
-    var selectPrice = "SELECT P_Price FROM product WHERE P_Id=?"
-    db.query(selectPrice, [body.P_Id], (err, result) => {
-        var Qty = body.Qty
-        var P_Price = 0;
-        var Discount = body.Discount
+    var select = "SELECT cart.Id,cart.Quantity,product.P_Price FROM cart "
+    select += " INNER JOIN product ON cart.Product_Id = product.P_Id "
+    select += " WHERE cart.Id=?"
+    db.query(select, [body.cart_id], (err, result) => {
 
         if (!err) {
-            result.map((element, index) => {
-                return P_Price = element.P_Price
-            })
-            var Total = (Number(Qty) * Number(P_Price))
-            const sql = "INSERT INTO `orderproduct`(`S_Id`,`User_Id`,`P_Id`,`Qty`,`Total`,`Discount`,`Grand_Total`,`Status`,`Description`,`Date_Post`) VALUES(?,?,?,?,?,?,?,?,?,?)"
-            const paramet = [null, body.User_Id, body.P_Id, Number(Qty), Number(Total), Number(Discount), (Total - (Total * Number(Discount))), 1, body.Description, new Date()]
-            db.query(sql, paramet, (err, row) => {
-                if (err) {
-                    res.json({
-                        error: true,
-                        message: err
-                    })
-                }
-                else {
-                    if (row.affectedRows > 0) {
+            if (result.length > 0) {
+                var cart = result[0].Id
+                var qty = result[0].Quantity
+                var price = result[0].P_Price
+                var Total = (qty * price)
+                var Discount = body.Discount
+                var Grand_Total = Total - (Total * Number(Discount) / 100)
+
+                const sql = "INSERT INTO `orderproduct`(`Order_product_Id`,`cart_id`,`order_num`,`Total`,`Discount`,`Grand_Total`,`order_status`,`payment_id`,`Description`,`Date_Post`) VALUES(?,?,?,?,?,?,?,?,?,?)"
+                const paramet = [null, cart, body.order_num, Total, body.Discount, Grand_Total, body.order_status, body.payment_id, body.Description, new Date()]
+                db.query(sql, paramet, (err, row) => {
+                    if (err) {
                         res.json({
-                            message: "Insert success!",
-                            RecordNum: row.length,
-                            data: row
-                        })
-                    } else {
-                        res.json({
-                            message: "Input no succes!",
+                            error: true,
+                            message: err
                         })
                     }
+                    else {
+                        if (row.affectedRows > 0) {
+                            res.json({
+                                message: "Insert success!",
+                            })
+                        } else {
+                            res.json({
+                                message: "Insert no succes!",
+                            })
+                        }
 
-                }
+                    }
 
-            })
+                })
+            } else {
+                res.json({
+                    error: true,
+                    message: "cart not found"
+                })
+            }
+
+
         } else {
             res.json({
                 error: true,
-                message: "Product not found"
+                message: "cart not found"
             })
         }
 
@@ -77,10 +85,8 @@ const CreateOrderProduct = (req, res) => {
 
 const getAll_OrderProduct = (req, res) => {
 
-    var sql ="SELECT O.S_Id, U.User_Name, P.P_Name,P.P_Price,O.Qty, O.Total, O.Discount, O.Grand_Total, O.Status, O.Description, DATE_FORMAT(O.Date_Post,'%d/%m/%Y %h:%i %p') AS Date_Post "
-        sql+=" FROM((orderproduct AS O INNER JOIN user AS U ON O.User_Id = U.User_Id) "
-        sql+=" INNER JOIN product AS P ON O.P_Id = P.P_Id) ORDER BY O.S_Id DESC "
-
+    var sql = "SELECT od.* FROM orderproduct od "
+    sql += " INNER JOIN cart ON od.cart_id = cart.Id "
     db.query(sql, (err, row) => {
 
         if (err) {
@@ -91,9 +97,7 @@ const getAll_OrderProduct = (req, res) => {
             })
         } else {
             if (row != 0) {
-
                 res.json({
-                    RecordNum: row.length,
                     list: row,
                 })
 
@@ -110,11 +114,14 @@ const getAll_OrderProduct = (req, res) => {
 }
 
 const getOne_OrderProduct = (req, res) => {
-    let id = req.params.id
-     var sql ="SELECT O.S_Id, U.User_Name, P.P_Name,P.P_Price,O.Qty, O.Total, O.Discount, O.Grand_Total, O.Status, O.Description,  DATE_FORMAT(O.Date_Post,'%d/%m/%Y %h:%i %p') AS Date_Post "
-        sql+=" FROM((`orderproduct` AS O INNER JOIN user AS U ON O.User_Id = U.User_Id) "
-        sql+=" INNER JOIN product AS P ON O.P_Id = P.P_Id) WHERE O.S_Id = ? "
-    db.query(sql, [id], (err, row) => {
+    let order_num = req.query.user_id
+    var sql = "SELECT od.*,p.P_Name,u.User_Name FROM orderproduct od "
+    sql += " INNER JOIN cart ON od.cart_id = cart.Id "
+    sql += " INNER JOIN product p ON cart.Id = p.P_Id "
+    sql += " INNER JOIN user u ON cart.User_Id = u.User_Id "
+    sql += " WHERE u.User_Id=?"
+    // sql += " WHERE od.Order_Product_Id=?"
+    db.query(sql, [order_num], (err, row) => {
         Row = row
         if (err) {
             res.json({
@@ -142,98 +149,10 @@ const getOne_OrderProduct = (req, res) => {
 
 }
 
-const Update_OrderProduct = (req, res) => {
-    let body = req.body
-    let message = {}
-    if (body.User_Id == null || body.User_Id == "") {
-        message.User_Id = "Plz input user Id"
-    }
-    if (body.Status == null || body.Status == "") {
-        message.Status = "Plz set Status"
-    }
-
-    if (Object.keys(message).length > 0) {
-        res.json({
-            error: true,
-            message: message
-        })
-        return false
-    }
-
-    const sql = "UPDATE `orderproduct` SET User_Id=?,Status=?,Description=?,Date_Post=? WHERE S_Id=?"
-    let paramet = [body.User_Id,body.Status, body.Description,new Date() , body.S_Id]
-    db.query(sql, paramet, (err, row) => {
-        if (err) {
-            res.json({
-                message: err,
-                error: true
-
-            })
-        } else {
-            if (row.affectedRows > 0) {
-                res.json({
-                    message: "You have Updated!",
-                    RecordNum: row.length,
-                    list: row
-
-                })
-            } else {
-                res.json({
-                    message: "Updated flase plz check again!",
-                    list: row
-
-                })
-            }
-
-        }
-    })
-
-}
-
-const Delete_OrderProduct = (req, res) => {
-
-    let body = req.body
-    let message = {}
-    if (body.S_Id == "" || body.S_Id == null || body.S_Id == undefined) {
-        message.P_Id = "Plz select sale to delete "
-    }
-    if (Object.keys(message).length > 0) {
-        res.json({
-            error: true,
-            message: message
-        })
-        return false
-    }
-
-    const sql = "DELETE FROM orderproduct WHERE S_Id=?"
-    db.query(sql, [body.S_Id], (err, row) => {
-        if (err) {
-            res.json({
-                message: err,
-                error: true
-            })
-        } else {
-            if (row.affectedRows > 0) {
-                res.json({
-                    message: `Deleted`,
-                    list: row
-                })
-            } else {
-                res.json({
-                    message: `ID not found`,
-                })
-            }
-
-        }
-    })
-
-}
 
 module.exports =
 {
     CreateOrderProduct,
     getAll_OrderProduct,
-    getOne_OrderProduct,
-    Update_OrderProduct,
-    Delete_OrderProduct
+    getOne_OrderProduct
 }
